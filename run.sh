2da2d2b8005b1b5634b5c28cd8266b8d8a59602d
Frank Hoeflich <frankh@fastmail.net>
2021-07-18 18:00:26 -0700

    Fill in basic steps of cloning poky in prep for creating build and target VMs.

--- run.sh
+++ run.sh
@@ -2,17 +2,40 @@
 #
 # run(1) -- run demo driver to check out, build, deploy and launch everything.
 #
-# 1. Check out demo driver repo
-# 2. Run (=> Yocto)
+# 1. Check out demo-driver repo via git on initiator machine (x86_64) by hand.
+# 2. Run run.sh (=> Yocto, qemu builds, ssh builds/installs)
 # 3. Demo driver checks out component repo(s):
-#	vanilla kernel, Frank's driver, dedicated machine or qemu VM
-# 4. Build kernel
-# 5. Build device driver via dkms
-# 6. Use dedicated machine OR build and launch vanilla VM
-# 7. Install augmented kernel and built-in driver or module on VM, reboot
-# 8. Demo device usage on machine/VM
+#	 dedicated machine or qemu VM(s),
+#    vanilla kernel,
+#    Frank's driver;
+# 4. Start x86_64 qemu VM for build (build VM)
+# 5. Build kernel on build VM
+# 6. Build device driver via dkms on build VM
+# 7. Start arm64 qemu VM for target (target VM)
+# 8. Install augmented kernel and built-in driver or module on VM, reboot
+# 9. Demo device usage on machine/VM
 #
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-echo "Hello, world"
+#
+# 1. Check out demo-driver repo via git on initiator machine (x86_64)
+#
+#git clone git@github.com:fhoeflich/demo-driver.git
+#cd demo-driver
+
+#
+# 2. Run run.sh (=> Yocto, qemu builds, ssh builds/installs)
+#
+echo 'Step 2: running run.sh ...'
+#./run.sh
+
+#
+# 3. Demo driver checks out component repo(s):
+#	 poky (to make build and target VMs),
+#    vanilla kernel,
+#    Frank's driver;
+#
+echo 'Step 3: cloning poky ...'
+git clone git://git.yoctoproject.org/poky
+
 exit 0
